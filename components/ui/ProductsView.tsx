99990d3fe11d854c65ae366dfd0411966ae10884
Suyash Ganesh Patil <patilsuyash892@gmail.com>
2024-11-22 20:05:11 +0530

    Fix

--- components/ui/ProductsView.tsx
+++ components/ui/ProductsView.tsx
@@ -4,17 +4,17 @@ import { ChevronsUpDown, Check } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandItem,
-    CommandList,
-    CommandInput,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+  CommandInput,
 } from "@/components/ui/command";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@/components/ui/popover";
 import type { Category } from "@/sanity.types";
 
@@ -22,57 +22,61 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 interface CategorySelectorProps {
-    categories: Category[];
-     
+  categories: {
+    data: Category[];
+  };
+
 }
 
-export default function CategorySelectorComponent({ categories }: CategorySelectorProps) {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState("");
 
-    return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-                <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-[200px] justify-between"
+export default function CategorySelectorComponent({ categories }: CategorySelectorProps) {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  console.log(categories);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? categories.data.find((category) => category.title === value)?.title
+            : "Select category..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search category..." />
+          <CommandList>
+            <CommandEmpty>No category found.</CommandEmpty>
+            <CommandGroup>
+              {categories.data.map((category) => (
+                <CommandItem
+                  key={category.value}
+                  value={category.title}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
                 >
-                    {value
-                        ? categories.find((category) => category.value === value)?.label
-                        : "Select category..."}
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-                <Command>
-                    <CommandInput placeholder="Search category..." />
-                    <CommandList>
-                        <CommandEmpty>No category found.</CommandEmpty>
-                        <CommandGroup>
-                            {categories.map((category) => (
-                                <CommandItem
-                                    key={category.value}
-                                    value={category.value}
-                                    onSelect={(currentValue) => {
-                                        setValue(currentValue === value ? "" : currentValue);
-                                        setOpen(false);
-                                    }}
-                                >
-                                    <Check
-                                        className={cn(
-                                            "mr-2 h-4 w-4",
-                                            value === category.value ? "opacity-100" : "opacity-0"
-                                        )}
-                                    />
-                                    {category.label}
-                                </CommandItem>
-                            ))}
-                        </CommandGroup>
-                    </CommandList>
-                </Command>
-            </PopoverContent>
-        </Popover>
-    );
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === category.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {category.title}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 }
